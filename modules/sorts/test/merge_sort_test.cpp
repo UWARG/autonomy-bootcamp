377b6ab7e0a806624f4c3cdf6515eb7fd50d843e
Oscar <ooosssososos@hotmail.com>
2016-09-27 19:43:17 -0400

    fixed text case names

--- modules/sorts/test/merge_sort_test.cpp
+++ modules/sorts/test/merge_sort_test.cpp
@@ -1,12 +1,13 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE merge_sort_test
+#define BOOST_TEST_MODULE sort_tests
 #include <boost/test/unit_test.hpp>
 #include "merge_sort.h" // declares type foo, header is in the core module
 #include <vector>
-
-BOOST_AUTO_TEST_CASE(TestName) {
+BOOST_AUTO_TEST_SUITE(merge_sort_test_suite);
+BOOST_AUTO_TEST_CASE(merge_sort_test_case1) {
     std::vector<int> uns {1, 5, 2 ,2 ,6, 99, 14, 21};
     std::vector<int> sorted {1, 2, 2, 5, 6, 14, 21, 99};
     merge_sort(uns);
     BOOST_CHECK(uns == sorted);
 }
+BOOST_AUTO_TEST_SUITE_END();
