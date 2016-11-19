dc455a31ee8359d6f0268439dc331c875b15601e
Tiger Zhao <tigerzhao.zhao@gmail.com>
2016-11-19 14:04:15 -0500

    fixed syntax of testsort

--- modules/mergesort/test/testsort.cpp
+++ modules/mergesort/test/testsort.cpp
@@ -7,7 +7,7 @@
 BOOST_AUTO_TEST_SUITE(merge_sort_test);
 BOOST_AUTO_TEST_CASE(merge_sort_test_case1) {
     int test[] = {-5,2,6,4,12,-7};
-    int testans[] = {-7,-5,2,4,6,12}
+    int testans[] = {-7,-5,2,4,6,12};
     merge_sort(test,sizeof(test)/sizeof(test[0]));
     BOOST_CHECK(std::equal(test,test+6,testans));
 }
