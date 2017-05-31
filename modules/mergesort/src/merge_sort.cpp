63db6cd4bbd0980868e12cda0fa439d8afd447c6
Murtaza Fatakdawala <murtazaaz98@gmail.com>
2017-05-31 14:54:34 -0400

    Added cpp version of mergesort

--- modules/mergesort/src/merge_sort.cpp
+++ modules/mergesort/src/merge_sort.cpp
@@ -1,4 +1,15 @@
-#include <iostream>
+/*
+ * This file is part of WARG's computer-vision
+ *
+ * Copyright (c) 2015-2016, Waterloo Aerial Robotics Group (WARG)
+ * All rights reserved.
+ *
+ * This software is licensed under a modified version of the BSD 3 clause license
+ * that should have been included with this software in a file called COPYING.txt
+ * Otherwise it is available at:
+ * https://raw.githubusercontent.com/UWARG/computer-vision/master/COPYING.txt
+ */
+
 #include <vector>
 
 std::vector<float> merge(std::vector<float>& a,std::vector<float> & b){
@@ -38,16 +49,17 @@ std::vector<float> merge_sort(std::vector<float>& a){
     return a;
 }
 int main(){
-    float arr[] = {14.2,14,7,6,18,10.2};
-    std::vector<float> v(arr ,arr + sizeof(arr)/sizeof(arr[0]) );
-
-    for(auto i = v.begin(); i !=v.end() ; i += 1){
-        std::cout<< *i <<' ';
-    }
-
-    merge_sort(v);
-    std::cout <<std::endl;
-    for(auto i = v.begin() ; i != v.end() ; i += 1){
-        std::cout<< *i <<' ';
-    }
+	// Following code was for debugging.
+    // float arr[] = {14.2,14,7,6,18,10.2};
+    // std::vector<float> v(arr ,arr + sizeof(arr)/sizeof(arr[0]) );
+      		
+    // for(auto i = v.begin(); i !=v.end() ; i += 1){
+    //     std::cout<< *i <<' ';
+    // }
+    // merge_sort(v);
+  
+    // std::cout <<std::endl;
+    // for(auto i = v.begin() ; i != v.end() ; i += 1){
+    //     std::cout<< *i <<' ';
+    // }
 }
