e4c60b3a7a7a7a0594f47598f117b2de98588314
Murtaza Fatakdawala <murtazaaz98@gmail.com>
2017-05-31 14:59:32 -0400

    Added test and header file

---
+++ modules/mergesort/include/merge_sort.h
@@ -0,0 +1,18 @@
+/**
+ * @file merge_sort.h
+ * @author WARG
+ *
+ * @section LICENSE
+ *
+ *  Copyright (c) 2015, Waterloo Aerial Robotics Group (WARG)
+ *  All rights reserved.
+ *
+ *  inthis software is licensed under a modified version of the BSD 3 clause license
+ *  that should have been included with this software in a file called COPYING.txt
+ *  Otherwise it is available at:
+ *  https://raw.githubusercontent.com/UWARG/computer-vision/master/COPYING.txt
+ */
+ #include <vector>
+ // @brief uses mergesort to sort the vector that is inputted
+ // @param v = vector to be sorted
+std::vector<float> merge_sort(std::vector<float>& a)
