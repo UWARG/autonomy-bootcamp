211f71329d8a4f4fc087b300ce263b2208a36515
Vignesh Sivan <vigsivan@Vigneshs-MacBook-Air.local>
2018-01-15 11:07:48 -0500

    modified CMakeLists

--- modules/sort/test/make/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /computer-vision-bootcamp/modules/sort/test
-# Build directory: /computer-vision-bootcamp/modules/sort/test/make
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
