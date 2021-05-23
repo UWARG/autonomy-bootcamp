73078e2a0d4a57bc6c0bd40d6865cdab54051b67
Michael Aizenshtadt <Michael@Michaels-MacBook-Pro.local>
2021-05-23 14:39:10 -0400

    Finished neural net

--- main.py
+++ main.py
@@ -12,5 +12,45 @@ Hints:
 
 # Import whatever libraries/modules you need
 import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-# Your working code here
+
+
+(trainImages, trainLabels), (testImages, testLabels) = datasets.cifar10.load_data()
+trainImages, testImages = trainImages/255.0, testImages/255.0 # normalizing the values
+
+# defining the convolutional base
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+
+# adding dense layers on top of convolutional base
+model.add(layers.Flatten())
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=['accuracy'])
+
+history = model.fit(trainImages, trainLabels, epochs=10, 
+    validation_data=(testImages, testLabels))
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
+
+test_loss, test_acc = model.evaluate(testImages,  testLabels, verbose=2)
+
+print(test_acc)
