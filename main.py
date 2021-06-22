921a1d1d052b29c2e5bb2bf41f6ac306140b136a
Justin Toft <justintoft10@gmail.com>
2021-06-21 20:46:02 -0400

    Completed Vision Bootcamp

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-"""
-This is a starter file to get you going. You may also include other files if you feel it's necessary.
-
-Make sure to follow the code convention described here:
-https://github.com/UWARG/computer-vision-python/blob/main/README.md#naming-and-typing-conventions
-
-Hints:
-* The internet is your friend! Don't be afraid to search for tutorials/intros/etc.
-* We suggest using a convolutional neural network.
-* TensorFlow Keras has the CIFAR-10 dataset as a module, so you don't need to manually download and unpack it.
-"""
-
-# Import whatever libraries/modules you need
-
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
 
-# Your working code here
+# Load the CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# We create a convolutional base using a stack of Conv2D and MaxPooling2D layers.
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+# Flatten the 3D tensor to 1D
+model.add(layers.Flatten())
+
+# Add dense layers on top
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+EPOCHS = 10
+# Train the model
+history = model.fit(train_images, train_labels, epochs=EPOCHS,
+                     validation_data=(test_images, test_labels))
+
+# Make a plot of loss and validation loss
+plt.plot(history.history['loss'], label='loss')
+plt.plot(history.history['val_loss'], label = 'val_loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.ylim([0.5, 1.5])
+plt.legend(loc='lower right')
+
+# Save file to plot.png
+plt.savefig("plot.png")
