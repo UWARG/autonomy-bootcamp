6cc3d02903eb09c516b5759415a322eb07bf082f
David Wang <62212652+enjineerMan@users.noreply.github.com>
2021-09-11 15:55:25 -0400

    67.37% validation accuracy, 20 epochs

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-cifar10 = tf.keras.datasets.cifar10 # 28x28 images of hand-written digits 0-9
+cifar10 = tf.keras.datasets.cifar10 
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -26,22 +26,28 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential() 
+
+# First convolution layer
 model.add(Conv2D(256, (3, 3), input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
+# second convolution layer
 model.add(Conv2D(256, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-
 model.add(Dense(64))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+# output layer
+model.add(Dense(10))
+model.add(Activation('softmax'))
 
 model.compile(optimizer='adam', # default oprimizer
              loss='sparse_categorical_crossentropy', 
              metrics=['accuracy']) #keep track of accuracy along the way
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=20)
+
+val_loss, val_acc = model.evaluate(x_test, y_test) #validate model
+print(val_loss, val_acc)
