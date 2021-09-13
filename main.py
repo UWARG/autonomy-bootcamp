d51f62fa94af466cf1683b00f07f97200e973eef
Ansh Sahny <anshsahny@gmail.com>
2021-09-12 21:06:20 -0400

    Completed Bootcamp

--- main.py
+++ main.py
@@ -12,6 +12,96 @@ Hints:
 
 # Import whatever libraries/modules you need
 
-import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import datasets, utils, layers, models
 
 # Your working code here
+
+# Load the CIFAR10 Dataset
+(trainX, trainY), (testX, testY) = datasets.cifar10.load_data()
+
+# Converting the labels into a new binary variable for the category
+trainY = utils.to_categorical(trainY)
+testY = utils.to_categorical(testY)
+
+# Convert from integer to float
+trainX = trainX.astype('float32')
+testX = testX.astype('float32')
+
+# Normalize the images to the range of 0 to 1
+trainX = trainX / 255.0
+testX = testX / 255.0
+
+# Constant Variables
+NUM_OF_CLASSES = 10
+HEIGHT = 32
+WIDTH = 32
+EPOCHS = 25
+BATCH_SIZE = 64
+
+# Next steps are initializing and developing the CNN model. The feature extraction part of the model is to be
+# created using a double convolutional layer, followed by a batch normalization layer and then a max pooling
+# layer. It will end with a dropout layer. This will be repeated 3 times with the first convolutional layer to
+# have 32 nodes, the second convolutional layer to have 64 nodes, and the third convolutional layer to have 128
+# nodes. The dropout layer will increase by 0.1 every time.
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(HEIGHT, WIDTH, 3)))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+model.add(layers.Dropout(0.2))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.MaxPooling2D((2, 2), padding='same'))
+model.add(layers.Dropout(0.4))
+
+# The model will then be flattened to enter the last hidden layer. The last hidden layer has 128 nodes, followed
+# by a Batch Normalization layer, a Dropout layer, then the output layer with 10 nodes and the number of classes
+# in the CIFAR10 dataset is 10
+
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.BatchNormalization())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model using the adam optimizer, categorical cross entropy loss for multi-class classification and
+# monitor accuracy value
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Training the model
+history = model.fit(trainX, trainY, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_data=(testX, testY))
+
+# Plot of both training and validation losses over epochs
+plt.plot(history.history['loss'], color='blue', label='Training Loss')
+plt.plot(history.history['val_loss'], color='red', label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(loc='lower right')
+plt.show()
+
+# Plot of both training and validation accuracy over epochs
+plt.plot(history.history['accuracy'], color='blue', label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], color='red', label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+
+# Evaluating the model with the test images to produce the final test accuracy value
+_, testAccuracy = model.evaluate(testX, testY)
+print('\nTEST ACCURACY: ' + '%.3f' % (testAccuracy * 100.0))
+x
