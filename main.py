5619d89d3f57eba71c0083f319c7e53d28754632
David Wang <62212652+enjineerMan@users.noreply.github.com>
2021-10-13 20:27:02 -0400

    organized code into functions

--- main.py
+++ main.py
@@ -19,35 +19,45 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 cifar10 = tf.keras.datasets.cifar10 
-
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
-
 model = tf.keras.models.Sequential() 
 
-# First convolution layer
-model.add(Conv2D(256, (3, 3), input_shape=x_train.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+def data_preparation():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# second convolution layer
-model.add(Conv2D(256, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+    x_train = tf.keras.utils.normalize(x_train, axis=1)
+    x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-model.add(Dense(64))
+def model_construction():
+    # First convolution layer
+    model.add(Conv2D(256, (3, 3), input_shape=x_train.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-# output layer
-model.add(Dense(10))
-model.add(Activation('softmax'))
+    # second convolution layer
+    model.add(Conv2D(256, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
-model.compile(optimizer='adam', # default oprimizer
-             loss='sparse_categorical_crossentropy', 
-             metrics=['accuracy']) #keep track of accuracy along the way
-model.fit(x_train, y_train, epochs=20)
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+    model.add(Dense(64))
 
-val_loss, val_acc = model.evaluate(x_test, y_test) #validate model
-print(val_loss, val_acc)
+    # output layer
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    model.compile(optimizer='adam', # default oprimizer
+                loss='sparse_categorical_crossentropy', 
+                metrics=['accuracy']) #keep track of accuracy along the way
+
+def model_training():
+    model.fit(x_train, y_train, epochs=20)
+
+def model_validation():
+    val_loss, val_acc = model.evaluate(x_test, y_test) #validate model
+    print(val_loss, val_acc)
+
+if __name__ == "__main__":
+    data_preparation()
+    model_construction()
+    model_training()
+    model_validation()
