6aa9bb7e6a48a24dbd04c21bc4a3024011b4ae11
Aung Khant Min <aungkhantmin2014@gmail.com>
2022-01-02 19:54:40 -0500

    initial commit

--- main.py
+++ main.py
@@ -12,6 +12,79 @@ Hints:
 
 # Import whatever libraries/modules you need
 
-import numpy as np
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
 
 # Your working code here
+
+# -- training and testing model --
+# load data 
+(xTrain, yTrain), (xTest,yTest) = datasets.cifar10.load_data()
+
+# reshape to 1D
+yTrain = yTrain.reshape(-1,) 
+
+# normalize data to be between 0 and 1
+xTrain = xTrain / 255.0
+xTest = xTest / 255.0
+
+# make convolutional network
+cnn = models.Sequential([
+    # convolution + pooling layer 1
+    layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    
+    # convolution + pooling layer 2
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+    # convolution + pooling layer 3
+    layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    # flatten
+    layers.Flatten(),
+
+    # fully connected layers
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# compile model
+cnn.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# train cnn with training data
+history = cnn.fit(xTrain, yTrain, epochs=10, validation_data=(xTest, yTest))
+
+# evaluate model with testing data
+cnn.evaluate(xTest, yTest)
+
+
+# -- plotting data -- 
+
+# divide plot into two subplots
+fig, (ax1, ax2) = plt.subplots(1, 2)
+# title
+fig.suptitle('Model Performance')
+
+# plot training accuracy
+ax1.plot(history.history["accuracy"])
+# plot validation accuracy
+ax1.plot(history.history["val_accuracy"])
+ax1.set_title("Accuracy")
+ax1.set(ylabel='Accuracy', xlabel='Epochs')
+ax1.legend(["Training", "Validation"])
+
+# plot training loss
+ax2.plot(history.history["loss"])
+# plot validation loss
+ax2.plot(history.history["val_loss"])
+ax2.set_title("Loss")
+ax2.set(ylabel='Loss', xlabel='Epochs')
+ax2.legend(["Training", "Validation"])
+
+plt.show()
+
