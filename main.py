1d556bd7023f9c425db1fa5d543bfe9e981984ae
Luca Bastone <lucab.mohabir@gmail.com>
2022-01-12 17:46:59 -0500

    Replaced  previous model with a convolutional neural network

--- main.py
+++ main.py
@@ -22,25 +22,29 @@ import matplotlib.pyplot as pyplot
 #Load CIFAR-10 Dataset
 (fitData, fitLabels), (valData, valLabels) = tf.keras.datasets.cifar10.load_data()
 
-# Create a Sequential model with appropriate input shape for data set (32x32 image w/ 3 color channels)
+# Create a basic convolutional neural network with a sequential model
 model = tf.keras.Sequential([
-    tf.keras.layers.Normalization(),
-    tf.keras.layers.Flatten(input_shape=(32, 32, 3)),
+    tf.keras.layers.Conv2D(32, (3, 3), input_shape=(32, 32, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+    tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dense(10)
 ])
 
-#Normalize data
+model.compile(optimizer='Adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
+
+# Normalize data
 fitData = fitData / 255.0
 valData = valData / 255.0
 
-model.compile(optimizer='Adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
-
 # Fit the model with the training data
-model.fit(fitData, fitLabels, epochs=100, validation_data=(valData, valLabels))
+model.fit(fitData, fitLabels, epochs=10, validation_data=(valData, valLabels))
 
 # Plot the loss with both fit data and validation data over epochs
-print(model.history.history.keys())
+pyplot.figure()
 
 pyplot.plot(model.history.history['loss'])
 pyplot.plot(model.history.history['val_loss'])
@@ -49,6 +53,19 @@ pyplot.title('Model Loss over Epochs')
 
 pyplot.xlabel('Epoch')
 pyplot.ylabel('Loss')
+
+pyplot.legend(['Training', 'Validation'])
+
+#Plot accuracy over epochs
+pyplot.figure()
+
+pyplot.plot(model.history.history['accuracy'])
+pyplot.plot(model.history.history['val_accuracy'])
+
+pyplot.title('Accuracy over Epochs')
+
+pyplot.xlabel('Epoch')
+pyplot.ylabel('Accuracy')
 
 pyplot.legend(['Training', 'Validation'])
 
