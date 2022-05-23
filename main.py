4207e7c27b1a41e6175ebc70a390418b49d6dc0a
Aksh4y2604 <63839864+Aksh4y2604@users.noreply.github.com>
2022-05-23 22:23:45 +0530

    Trained the model and attatched the results

--- main.py
+++ main.py
@@ -11,7 +11,89 @@ Hints:
 """
 
 # Import whatever libraries/modules you need
-
+import matplotlib.pyplot as plt
 import numpy as np
 
-# Your working code here
+import ssl
+
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# ssl Verification error fix 
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Loading the data-set and normalizing the rgb values between 0 and 1
+(trainImages, trainLables), (testImages, testLables) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+trainImages, testImages = trainImages / 255.0, testImages / 255.0
+
+# Verifying the data 
+classNames = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+## Defining a function to plot the dataset(for testing)
+def plot_image(i, predictionsArray, trueLabel, img):
+    trueLabel, img = trueLabel[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predictedLabel = np.argmax(predictionsArray)
+    if predictedLabel == trueLabel:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(classNames[predictedLabel],
+                                    100*np.max(predictionsArray),
+                                    classNames[trueLabel]),
+                                    color=color)
+
+#Creating a Convolutional Neural Network. 
+model = models.Sequential()
+
+#Defining the convulational a stack of COnv2D and MaxPolling2D layers
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+#Displaying the model architecture
+print("Before Flattening: ")
+model.summary()
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
+
+print("After Flattening: ")
+model.summary()
+
+#Compiling and training the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(trainImages, trainLables, epochs=10, 
+                    validation_data=(testImages, testLables))
+
+#Evaluating the model using the test dataset and measuring the accuracy
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+testLoss, testAccuracy = model.evaluate(testImages,  testLables, verbose=2)
+
+print("Test Loss: ", testLoss)
+print("Test Accuracy: ", testAccuracy)
+
+'''  Command line results attatched in results.txt file. 
+ Had a lot of fun doing this and learnt alot. Let me know the things I could improve upon.
+ Excited to hopefully join the team and learn more. :)  '''
