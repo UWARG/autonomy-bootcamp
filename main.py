1698b239e3ebe2ed4d185d7edbb908f74e55d167
ebwu95 <38186996+ebwu95@users.noreply.github.com>
2022-09-14 15:34:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,98 @@
-"""
-This is a starter file to get you going. You may also include other files if you feel it's necessary.
-
-Make sure to follow the code convention described here:
-https://github.com/UWARG/computer-vision-python/blob/main/README.md#naming-and-typing-conventions
-
-Hints:
-* The internet is your friend! Don't be afraid to search for tutorials/intros/etc.
-* We suggest using a convolutional neural network.
-* TensorFlow Keras has the CIFAR-10 dataset as a module, so you don't need to manually download and unpack it.
-"""
-
-# Import whatever libraries/modules you need
-
+import torch
+from matplotlib import pyplot as plt
 import numpy as np
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from matplotlib import pyplot as plt
 
-# Your working code here
+transform = transforms.Compose([transforms.ToTensor(),
+                                      transforms.Normalize([0.5, 0.5, 0.5],
+                                                            [0.5, 0.5, 0.5])])
+
+#downloading data                                               
+train_data = datasets.CIFAR10('data', train=True,
+                              download=True, transform=transform)
+test_data = datasets.CIFAR10('data', train=False,
+                             download=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+testloader = torch.utils.data.DataLoader(test_data, batch_size=32, shuffle=True)
+  
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#defining the model architecture
+model = nn.Sequential(nn.Conv2d(3, 32, 3, padding=1),
+                      nn.ReLU(),
+                      nn.MaxPool2d(2,2),
+                      nn.Conv2d(32, 64, 3, padding=1),
+                      nn.ReLU(),
+                      nn.MaxPool2d(2,2),
+                      nn.Conv2d(64, 128, 3, padding=1),
+                      nn.ReLU(),
+                      nn.MaxPool2d(2,2),
+                      nn.Flatten(1, 3),
+                      nn.Linear(128 * 4 * 4, 1000),
+                      nn.ReLU(),
+                      nn.Dropout(0.3),
+                      nn.Linear(1000, 500),
+                      nn.ReLU(),
+                      nn.Dropout(0.3),
+                      nn.Linear(500, 250),
+                      nn.ReLU(),
+                      nn.Dropout(0.3),
+                      nn.Linear(250, 10),
+                      nn.LogSoftmax(dim=1))
+
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr = 0.05)
+model.to(device)
+
+epochs = 7
+
+#training
+train_history = []
+valid_history = []
+for e in range(epochs):
+    train_loss = 0
+    valid_loss = 0
+    for images, labels in trainloader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        logits = model.forward(images)
+        loss = criterion(logits, labels)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+    else:
+        accuracy = 0
+        model.eval()
+        #validation
+        with torch.no_grad():
+            for images, labels in testloader:
+                images, labels = images.to(device), labels.to(device)
+                logits = model.forward(images)
+                ps = torch.exp(logits)
+                top_probability, top_class = ps.topk(1, dim=1)
+                equal = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equal.type(torch.FloatTensor))
+                loss = criterion(logits, labels)
+                valid_loss += loss.item()
+        model.train()
+        train_history.append(train_loss/len(trainloader))
+        valid_history.append(valid_loss/len(testloader))
+        print(f"Epoch {e+1}/{epochs}")
+        print(f"Test Accuracy {100 * accuracy/(len(testloader))}%")
+        print(f"Train Loss {train_loss/len(trainloader)}")
+        print(f"Valid Loss {valid_loss/len(testloader)}")
+
+#plotting
+plt.plot(train_history)
+plt.plot(valid_history)
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
