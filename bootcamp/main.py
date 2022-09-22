ca455291954c2c11bd896667aa34b1fd9a077320
MrBruh <mdnssv@gmail.com>
2022-09-21 21:51:11 -0400

    Messing around with the data sets

--- bootcamp/main.py
+++ bootcamp/main.py
@@ -12,13 +12,62 @@ Hints:
 
 # Import whatever libraries/modules you need
 
+from typing import Dict
 import numpy as np
 
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
 # Your working code here
 
-if __name__ == "__main__":
+def main():
+    """Main entry point to the program"""
+
     print(f"Using {'cuda' if torch.cuda.is_available() else 'cpu'} device")
+
+    training_data = torchvision.datasets.CIFAR10(
+        root="data",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor()
+    )
+
+    test_data = torchvision.datasets.CIFAR10(
+        root="data",
+        train=False,
+        download=True,
+        transform=transforms.ToTensor()
+    )
+
+    labels_map: Dict[int, str] = {
+        0: "Plane",
+        1: "Car",
+        2: "Bird",
+        3: "Cat",
+        4: "Deer",
+        5: "Dog",
+        6: "Frog",
+        7: "Horse",
+        8: "Ship",
+        9: "Truck",
+    }
+
+    figure = plt.figure(figsize=(8, 8))
+    cols, rows = 3, 3
+    for i in range(1, cols * rows + 1):
+        sample_idx = torch.randint(len(training_data), size=(1,)).item()
+        img, label = training_data[sample_idx]
+        figure.add_subplot(rows, cols, i)
+        plt.title(labels_map[label])
+        plt.axis("off")
+        plt.imshow(img.permute(1, 2, 0).squeeze(), cmap="gray")
+    plt.show()
+
+    print('lol')
+
+
+# Excecute the main entry point
+if __name__ == "__main__":
+    main()
