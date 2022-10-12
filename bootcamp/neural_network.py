9323e30e1eed780408511f6e73e5c659b21ad9da
MrBruh <mdnssv@gmail.com>
2022-10-12 19:57:55 -0400

    Added documentation to NeuralNetwork

--- bootcamp/neural_network.py
+++ bootcamp/neural_network.py
@@ -5,19 +5,46 @@ import torch.nn as nn
 
 
 class NeuralNetwork(nn.Module):
-    """Class defining the structure and handling of a neural network"""
+    """
+    Class defining the structure and handling of a neural network.
 
-    def __init__(self) -> None:
+    Attributes
+    ----------
+    network: nn.Sequential
+        Defines the structure of the neural network
+
+    __kernel_size: int
+    __padding: int
+
+    Methods
+    -------
+    _init_()
+        Constructs the network architecture.
+    forward(input_tensor: torch.Tensor) -> torch.Tensor:
+        Steps the training forwards through the network architecture.
+    """
+
+    def __init__(self):
+        """
+        Constructs the network architecture.
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        NeuralNetwork
+        """
         super().__init__()
 
         # Make sure that padding = kernel_size // 2 to keep the same image size
-        self.kernel_size: int = 3
-        self.padding: int = 1
+        self.__kernel_size: int = 3
+        self.__padding: int = 1
         # 3 and 1 work best out of the pairs (3, 1), (5, 2), (7, 3)
 
         def conv_layer(in_channels: int, out_channels: int) -> nn.Sequential:
             return nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=self.kernel_size, padding=self.padding),
+                nn.Conv2d(in_channels, out_channels, kernel_size=self.__kernel_size, padding=self.__padding),
                 nn.ReLU(),
                 # I need to learn more about batch normalization, but adding it greatly speeds up training
                 nn.BatchNorm2d(out_channels),
@@ -45,13 +72,18 @@ class NeuralNetwork(nn.Module):
             linear_layers()
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Steps the training forwards through the network architecture
-
-        Args:
-            x (torch.Tensor): Input tensor
-
-        Returns:
-            torch.Tensor: Most likely classificiation
+    def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
         """
-        return self.network(x)
+        Steps the training forwards through the network architecture.
+
+        Parameters
+        ----------
+        input_tensor: torch.Tensor
+            Input tensor
+
+        Returns
+        -------
+        torch.Tensor:
+            Most likely classificiation
+        """
+        return self.network(input_tensor)
