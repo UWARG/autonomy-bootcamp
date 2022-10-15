c63bb1d352f327f048ae7fae4b5750983b78201d
avikam03 <avikam03@gmail.com>
2022-10-15 01:46:59 -0400

    completed bootcamp

--- main.py
+++ main.py
@@ -12,6 +12,262 @@ Hints:
 
 # Import whatever libraries/modules you need
 
-import numpy as np
+import os
+import torch
+import torchvision
+import tarfile
+from torchvision.datasets.utils import download_url
+from torch.utils.data import random_split
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor
+import matplotlib
+import matplotlib.pyplot as plt
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data.dataloader import DataLoader
 
-# Your working code here
+
+"""
+Downloads dataset from fast-ai's repository and extracts it to the data folder.
+Also transforms the data into PyTorch tensors.
+"""
+def download_data():
+    # Dowload the dataset
+    dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz"
+    download_url(dataset_url, '.')
+
+    # Extract from archive
+    with tarfile.open('./cifar10.tgz', 'r:gz') as tar:
+        tar.extractall(path='./data')
+
+    data_dir = './data/cifar10'
+
+    dataset = ImageFolder(data_dir+'/train', transform=ToTensor())
+    return dataset
+
+
+"""
+This function splits the dataset into a validation and training set.
+
+params:
+    dataset: the dataset to split
+    val_pct: the percentage of the dataset to use for validation
+
+returns:
+    train_dl: the training set
+    val_dl: the validation set
+"""
+def split_data(dataset, val_pct=0.1):
+    random_seed = 42 # done so that results can be reproduced
+    torch.manual_seed(random_seed)
+
+    val_size = int(len(dataset) * val_pct);
+    train_size = len(dataset) - val_size
+
+    train_ds, val_ds = random_split(dataset, [train_size, val_size])
+
+    batch_size=128
+
+    train_dl = DataLoader(train_ds, batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_dl = DataLoader(val_ds, batch_size*2, num_workers=4, pin_memory=True)
+
+    return train_dl, val_dl
+
+
+# wrapper function
+def init_data():
+    dataset = download_data()
+    return split_data(dataset)
+
+"""
+This class contains helper functions for training and validating the model.
+It is created by extending the nn.Module class.
+"""
+class ImageClassificationBase(nn.Module):
+    # Function for training
+    def training_step(self, batch):
+        images, labels = batch 
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    # Function for Validation
+    def validation_step(self, batch):
+        images, labels = batch 
+        out = self(images)                    # Generate predictions
+        loss = F.cross_entropy(out, labels)   # Calculate loss
+        acc = accuracy(out, labels)           # Calculate accuracy
+        return {'val_loss': loss.detach(), 'val_acc': acc}
+        
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()   # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()      # Combine accuracies
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item()}
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['train_loss'], result['val_loss'], result['val_acc']))
+
+
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+
+"""
+This class uses the nn.Sequential to create a cnn model.
+"""
+class Cifar10CnnModel(ImageClassificationBase):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 64 x 16 x 16
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 128 x 8 x 8
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), # output: 256 x 4 x 4
+
+            nn.Flatten(), 
+            nn.Linear(256*4*4, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10))
+        
+    def forward(self, xb):
+        return self.network(xb)
+
+
+# Picks GPU if it is available and cpu if it's not
+def get_default_device():
+    if torch.cuda.is_available():
+        return torch.device('cuda')
+    else:
+        return torch.device('cpu')
+
+
+# Moves tensors to chosen device    
+def to_device(data, device):
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+class DeviceDataLoader():
+    # Wrap a dataloader to move data to a device
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        # Yield a batch of data after moving it to device
+        for b in self.dl: 
+            yield to_device(b, self.device)
+
+    def __len__(self):
+        # Number of batches
+        return len(self.dl)
+
+"""
+This function evaluates the performance of the model against the validation set.
+
+params:
+    model: the model to evaluate
+    val_loader: the validation set
+
+returns:
+    result: a dictionary containing the loss and accuracy of the model for the current epoch
+"""
+@torch.no_grad()
+def evaluate(model, val_loader):
+    model.eval()
+    outputs = [model.validation_step(batch) for batch in val_loader]
+    return model.validation_epoch_end(outputs)
+
+
+
+"""
+This function trains the model for a given number of epochs. 
+
+params:
+    model: the model to train
+    train_loader: the training set
+    val_loader: the validation set
+    epochs: the number of epochs to train for
+    opt_func: the optimizer to use
+    lr: the learning rate
+
+returns:
+    history: a list of dictionaries containing the training and validation loss and accuracy
+"""
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    history = []
+    optimizer = opt_func(model.parameters(), lr)
+    for epoch in range(epochs):
+        # Training Phase 
+        model.train()
+        train_losses = []
+        for batch in train_loader:
+            loss = model.training_step(batch)
+            train_losses.append(loss)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        # Validation phase
+        result = evaluate(model, val_loader)
+        result['train_loss'] = torch.stack(train_losses).mean().item()
+        model.epoch_end(epoch, result)
+        history.append(result)
+    return history
+
+# Plots Accuracy vs. No. of epochs graph
+def plot_accuracies(history):
+    accuracies = [x['val_acc'] for x in history]
+    plt.plot(accuracies, '-x')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.title('Accuracy vs. No. of epochs')
+
+# Plots Loss vs. No. of epochs graph
+def plot_losses(history):
+    train_losses = [x.get('train_loss') for x in history]
+    val_losses = [x['val_loss'] for x in history]
+    plt.plot(train_losses, '-bx')
+    plt.plot(val_losses, '-rx')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.legend(['Training', 'Validation'])
+    plt.title('Loss vs. No. of epochs')
+
+
+def main():
+    train_dl, val_dl = init_data()
+
+    device = get_default_device()
+    print(device)
+    train_dl = DeviceDataLoader(train_dl, device)
+    val_dl = DeviceDataLoader(val_dl, device)
+    model = to_device(Cifar10CnnModel(), device)
+
+    history = fit(10, 0.001, model, train_dl, val_dl, torch.optim.Adam)
+
+    # Plot accuracy and loss
+    plot_accuracies(history)
+    plot_losses(history)
+
+
+if __name__ == "__main__":
+    main()
