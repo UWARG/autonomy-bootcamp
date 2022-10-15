d1ef3389f4895c89cf5e55d290ed6c86514c1d7a
Ishman Mann <ishman03@gmail.com>
2022-10-15 13:08:10 -0600

    edited misc code

--- misc_code.py
+++ misc_code.py
@@ -13,15 +13,15 @@
 
 
 #-------------------------------------------------------------------------------------------
-#Viewing augmented images
+# Viewing augmented images
 
 # image, label = next(iter(datasetTrain))
-# _ = label = datasetInfo.features['label'].int2str(label)
-# _ = image = dataResizingRescaling(image)
-# _ = imageAugmented = dataAugmention(image)
-# _ = plt.figure(0)
-# _ = plt.imshow(image)
-# _ = plt.title(label)
-# _ = plt.figure(1)
-# _ = plt.imshow(imageAugmented)
-# _ = plt.title(label)
+# label = datasetInfo.features['label'].int2str(label)
+# image = dataResizingRescaling(image)
+# imageAugmented = dataAugmention(image)
+# plt.figure(0)
+# plt.imshow(image)
+# plt.title(label)
+# plt.figure(1)
+# plt.imshow(imageAugmented)
+# plt.title(label)
