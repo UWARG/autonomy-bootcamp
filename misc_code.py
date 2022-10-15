fd387b13c9e7ab97b20ad0a63d929b6c74964758
Ishman Mann <ishman03@gmail.com>
2022-10-15 00:56:40 -0600

    added misc_code file

---
+++ misc_code.py
@@ -0,0 +1,12 @@
+# https://www.tensorflow.org/datasets/overview
+# iterating over a dataset
+
+# ds = datasetTrain.take(1)
+
+# for image, label in tfds.as_numpy(ds):
+#   print(type(image), type(label), label)
+#   plt.imshow(image)
+#   plt.title(datasetInfo.features['label'].int2str(label))
+
+
+# print(datasetInfo)
