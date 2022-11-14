b500cdc0fef175475e98c0da6bc56f1fe73d3b0e
Valentio Iverson <67735789+valentioiverson@users.noreply.github.com>
2022-11-13 21:24:15 -0500

    update main.py with code

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
-"""
-This is a starter file to get you going. You may also include other files if you feel it's necessary.
+# Import necessary libraries
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras import datasets, layers, models
 
-Make sure to follow the code convention described here:
-https://github.com/UWARG/computer-vision-python/blob/main/README.md#naming-and-typing-conventions
+# Loading data
+(trainImage, trainLabel), (testImage, testLabel) = datasets.cifar10.load_data()
 
-Hints:
-* The internet is your friend! Don't be afraid to search for tutorials/intros/etc.
-* We suggest using a convolutional neural network.
-* TensorFlow Keras has the CIFAR-10 dataset as a module, so you don't need to manually download and unpack it.
-"""
+# Normalize pixel values
+trainImage, testImage = trainImage/255.0, testImage/255.0
 
-# Import whatever libraries/modules you need
+# Available Class Names
+className = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+                'dog', 'frog', 'horse', 'ship', 'truck']
 
-import numpy as np
+# Create and Train the model
+# Define Sequential model 
+model = models.Sequential()
+# Create a 2D Convolutional Layer with 32 nodes, with a kernel size of (3,3), 
+# activation function relu and input shape of (32, 32, 3)
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+# Create a MaxPooling 2D Layer of pool size (2,2)
+model.add(layers.MaxPooling2D((2, 2)))
+# Create a 2D Convolutional Layer with 64 nodes, with a kernel size of (3,3), 
+# activation function relu
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# Create a MaxPooling 2D Layer of pool size (2,2)
+model.add(layers.MaxPooling2D((2, 2)))
+# Create a 2D Convolutional Layer with 64 nodes, with a kernel size of (3,3), 
+# activation function relu
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# Flattens the output into 1 dimension
+model.add(layers.Flatten())
+# Add dense layers
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10))
 
-# Your working code here
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+# Fit the model to our training data, and validate it with our test data. 
+history = model.fit(trainImage, trainLabel, epochs=10, 
+                    validation_data=(testImage, testLabel))
+
+# Plot the accuracy
+plt.plot(history.history['accuracy'], label ='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
+# Evaluate the accuracy of the model
+test_loss, test_acc = model.evaluate(testImage,  testLabel, verbose=2)
