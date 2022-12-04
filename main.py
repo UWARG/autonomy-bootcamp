679a83d68c9936ec83aa1a43f9d8a84509428431
Gary Shen <garyshen07@gmail.com>
2022-12-03 20:52:26 -0500

    Adding more convulation layers and batch normalization to improve accuracy

--- main.py
+++ main.py
@@ -15,16 +15,35 @@ import torch.optim as optim
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+
+        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+
+        self.conv6 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
+
+
+        self.fc1 = nn.Linear(256 * 4 * 4, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu((self.conv1(x)))
+        x = self.bn2(self.pool(F.relu(self.conv2(x)))) #64 x 16 x 16
+        x = F.relu((self.conv3(x)))
+        x = self.bn4(self.pool(F.relu(self.conv4(x)))) #128 x 8 x 8
+        x = F.relu((self.conv5(x)))
+        x = self.bn6(self.pool(F.relu(self.conv6(x)))) #256 x 4 x 4
         x = torch.flatten(x, 1) 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -53,10 +72,10 @@ def get_loss():
 def get_optimizer(net):
     return optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-def plot(value):
+def plot(value, epoch):
     plt.plot(value)
     plt.xlabel("EPOCH")
-    plt.xticks(np.arange(0,10 ,1))
+    plt.xticks(np.arange(0,epoch ,1))
     plt.title("Validation Accuracy")
     plt.show()
 
@@ -97,8 +116,8 @@ def train(net, train_loader, test_loader, epochs):
     return accuracies
 
 if __name__ == "__main__":
-    __epoch = 10
+    __epoch = 5
     __train_loader, __test_loader = load()
     net = Net()
     accuracies = train(net, __train_loader, __test_loader, __epoch)
-    plot(accuracies)
+    plot(accuracies, __epoch)
