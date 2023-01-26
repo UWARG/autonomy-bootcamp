4e4678007168e51e3bcf90fd8b85e2aaa8dcd33b
Alan Chen <chenalan2002@gmail.com>
2023-01-25 21:05:02 -0500

    Added Images

--- main.py
+++ main.py
@@ -1,156 +1,216 @@
 # Imports
-
-from torch import flatten, optim, nn, utils, device, no_grad, max, device
-from torchvision import transforms, datasets
-
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+from torch import device, flatten, max, nn, no_grad, optim, utils
+from torchvision import transforms, datasets
+
 class CNN(nn.Module):
-
+    """
+    Implementation of Convolutional Image Classifier.
+    """
     def __init__(self):
-
+        """
+        Layer Definitions.
+        """
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 12, 5, padding="same")
-        self.conv2 = nn.Conv2d(12, 12, 5, padding="same")
-        self.batch_norm1 = nn.BatchNorm2d(12)
-        self.pool1 = nn.MaxPool2d(2, 2)
 
-        self.conv3 = nn.Conv2d(12, 24, 5, padding="same")
-        self.conv4 = nn.Conv2d(24, 24, 5, padding="same")
-        self.batch_norm2 = nn.BatchNorm2d(24)
-        self.pool2 = nn.MaxPool2d(2, 2)
+        # Convolution Layers
+        self.conv1 =nn.Conv2d(3, 24, 5, padding="same")
+        self.conv2 =nn.Conv2d(24, 24, 5, padding="same")
+        self.conv3 =nn.Conv2d(24, 48, 3, padding="same")
+        self.conv4 =nn.Conv2d(48, 48, 3, padding="same")
+        self.conv5 =nn.Conv2d(48, 96, 3, padding="same")
+        self.conv6 =nn.Conv2d(96, 96, 3, padding="same")
 
-        self.conv5 = nn.Conv2d(24, 36, 3, padding="same")
-        self.conv6 = nn.Conv2d(36, 36, 3, padding="same")
-        self.batch_norm3 = nn.BatchNorm2d(36)
-        self.pool3 = nn.MaxPool2d(2, 2)
+        # Batch Normalization Layers
+        self.batch_norm1 =nn.BatchNorm2d(24)
+        self.batch_norm2 =nn.BatchNorm2d(48)
+        self.batch_norm3 =nn.BatchNorm2d(96)
 
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.flatten = nn.Flatten()
+        # Pooling Layers
+        self.pool1 =nn.MaxPool2d(2, 2)
+        self.pool2 =nn.MaxPool2d(2, 2)
+        self.pool3 =nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(36, 120)
-        self.fc2 = nn.Linear(120, 60)
-        self.fc3 = nn.Linear(60, 10)
+        # Global Max Pooling Layer
+        self.max_pool =nn.AdaptiveMaxPool2d(1)
+        self.flatten =nn.Flatten()
 
-        self.dropout1 = nn.Dropout(0.3)
-        self.dropout2 = nn.Dropout(0.2)
-        self.relu = nn.ReLU()
+        # Fully Connected Layers
+        self.fc1 =nn.Linear(96, 256)
+        self.fc2 =nn.Linear(256, 512)
+        self.fc3 =nn.Linear(512, 10)
+
+        # Dropout and Relu Layers
+        self.dropout1 =nn.Dropout(0.2)
+        self.dropout2 =nn.Dropout(0.2)
+        self.relu =nn.ReLU()
 
     def forward(self, x):
+        """
+        Model ARchitecture.
+        """
+        # Convolution layers before first downsampling
+        x =self.conv1(x)
+        x =self.batch_norm1(x)
+        x =self.relu(x)
+        x =self.conv2(x)
+        x =self.batch_norm1(x)
+        x =self.relu(x)
+        x =self.pool1(x)
 
-        x = self.conv1(x)
-        x = self.batch_norm1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.batch_norm1(x)
-        x = self.relu(x)
-        x = self.pool1(x)
+        # Convolution layers before second downsampling
+        x =self.conv3(x)
+        x =self.batch_norm2(x)
+        x =self.relu(x)
+        x =self.conv4(x)
+        x =self.batch_norm2(x)
+        x =self.relu(x)
+        x =self.pool2(x)
 
-        x = self.conv3(x)
-        x = self.batch_norm2(x)
-        x = self.relu(x)
-        x = self.conv4(x)
-        x = self.batch_norm2(x)
-        x = self.relu(x)
-        x = self.pool2(x)
+        # Convolution layers before third downsampling
+        x =self.conv5(x)
+        x =self.batch_norm3(x)
+        x =self.relu(x)
+        x =self.conv6(x)
+        x =self.batch_norm3(x)
+        x =self.relu(x)
+        x =self.pool3(x)
 
-        x = self.conv5(x)
-        x = self.batch_norm3(x)
-        x = self.relu(x)
-        x = self.conv6(x)
-        x = self.batch_norm3(x)
-        x = self.relu(x)
-        x = self.pool3(x)
+        # Global Max Pooling
+        x =self.max_pool(x)
+        x =self.flatten(x)
 
-        x = self.max_pool(x)
-        x = self.flatten(x)
-
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.dropout1(x)
-        x = self.fc2(x)
-        x = self.relu(x)
-        x = self.dropout2(x)
-        x = self.fc3(x)
+        # Fully Connected Layers
+        x =self.fc1(x)
+        x =self.relu(x)
+        x =self.dropout1(x)
+        x =self.fc2(x)
+        x =self.relu(x)
+        x =self.dropout2(x)
+        x =self.fc3(x)
 
         return x
 
 
-def fit(model, device, epochs, train_loader, val_loader):
+def fit(model, device, epochs, train_loader, val_loader, criterion, optimizer):
+    """
+    Method to train model
+    """
+    # Arrays to track statistics
+    accuracies =np.zeros(epochs)
+    losses =np.zeros(epochs)
+    accuracies_val =np.zeros(epochs)
+    losses_val =np.zeros(epochs)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters())
-    accuracy_sum = np.zeros(epochs)
-    accuracies = np.zeros(epochs)
-    losses_sum = np.zeros(epochs)
-    losses = np.zeros(epochs)
-
+    # Epoch loop
     for epoch in range(epochs):
 
+        # Enables training
         model.train()
 
-        # Training
+        # Training step
         for i, data in enumerate(train_loader):
 
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
+            inputs, labels =data
+            inputs, labels =inputs.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            outputs =model(inputs)
+            loss =criterion(outputs, labels)
+            # Back Propogation
             loss.backward()
             optimizer.step()
 
-        # Validation calculations
+        # Generate loss and accuracy statistics
         with no_grad():
+
+            # Calculate sum of training loss and accuracy
+            for i, data in enumerate(train_loader):
+
+                # Get prediction on training data
+                inputs, labels =data
+                inputs, labels =inputs.to(device), labels.to(device)
+                outputs =model(inputs)
+                predicted =max(outputs,dim=1)[1]
+
+                # Get metrics
+                accuracy =(predicted ==labels).sum()
+                accuracies[epoch] +=accuracy
+                loss = criterion(outputs, labels)
+                losses[epoch] +=loss
+
+            # Calculate sum of validation loss and accuracy
             for i, data in enumerate(val_loader):
 
+                # Get prediction on validation data
                 inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device)
-                outputs = model(inputs)
-                predicted = max(outputs,dim=1)[1]
+                outputs =model(inputs)
+                predicted =max(outputs,dim=1)[1]
 
-                accuracy = (predicted == labels).sum()
-                accuracy_sum[epoch] += accuracy
+                # Get metrics
+                accuracy =(predicted ==labels).sum()
+                accuracies_val[epoch] +=accuracy
+                loss =criterion(outputs, labels)
+                losses_val[epoch] +=loss
 
-                loss = criterion(outputs, labels)
-                losses_sum[epoch] += loss
+        # Calculate training loss and accuracy from sum
+        len_train_data =len(train_loader.dataset)
+        accuracies[epoch] /=len_train_data
+        losses[epoch] /=len_train_data
 
-        accuracies[epoch] = (accuracy_sum[epoch] / len(val_loader.dataset))
-        losses[epoch] = (losses_sum[epoch] / len(val_loader.dataset))
+        # Calculate validation loss and accuracy from sum
+        len_val_data =len(val_loader.dataset)
+        accuracies_val[epoch] /=len_val_data
+        losses_val[epoch] /=len_val_data
 
-        print(f"epoch {epoch}: {accuracies[epoch]} accuracy, {losses[epoch]} loss")
+        # Print current epoch stats
+        print(f"epoch {epoch}. acc: {accuracies[epoch]}, val_acc: \
+            {accuracies_val[epoch]}, loss: {losses[epoch]}, val_loss: {losses_val[epoch]}")
 
-    return losses, accuracies
+    return losses, accuracies, losses_val, accuracies_val
 
-def plot_results(losses, accuracies):
-
-    plt.plot(losses, label="loss over epochs")
+def plot_results(losses, accuracies, losses_val, accuracies_val):
+    """
+    Plot metrics for training and validation
+    """
+    # Plot loss
+    plt.plot(losses, label="loss")
+    plt.plot(losses_val, label="validation loss")
     plt.ylabel("loss")
     plt.xlabel("epoch")
+    plt.legend()
     plt.show()
 
-    plt.plot(accuracies, label="accuracy over epochs")
+    # Plot accuracy
+    plt.plot(accuracies, label="accuracy")
+    plt.plot(accuracies_val, label="validation accuracy")
     plt.ylabel("accuracy")
     plt.xlabel("epoch")
+    plt.legend()
     plt.show()
 
 if __name__ == "__main__":
 
+    # Load Data
+    BATCH_SIZE = 128
     transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
-
-    BATCH_SIZE = 50
-
     train_set = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     train_loader = utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
     val_set = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     val_loader = utils.data.DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
 
+    # Instantiate model
     DEVICE = device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = CNN().to(DEVICE)
-    losses, accuracies = fit(model, DEVICE, 10, train_loader, val_loader)
-    plot_results(losses, accuracies)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
+
+    # Train model
+    losses, accuracies, losses_val, accuracies_val = fit(model, DEVICE, 10, train_loader, val_loader, criterion, optimizer)
+    plot_results(losses, accuracies, losses_val, accuracies_val)
