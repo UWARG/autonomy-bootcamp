55258c750983f783155d531d57a698e0796067b1
kiarashmajdi <kiamaj.ca@gmail.com>
2023-02-05 22:27:30 -0500

    first commit of main.py

--- main.py
+++ main.py
@@ -14,4 +14,50 @@ Hints:
 
 import numpy as np
 
-# Your working code here
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+
+# Normalize the data
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# Define the model
+model = keras.Sequential([
+    keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D((2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# Evaluate the model
+test_loss, test_accuracy = model.evaluate(x_test, y_test)
+print("Test Loss: ", test_loss)
+print("Test Accuracy: ", test_accuracy)
+
+# Plot the training and validation accuracy over epochs
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+#%%
+
+#%%
