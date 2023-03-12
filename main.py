50ba2ddfbfb6b8c3a8d9022b6df82c822a53a0ea
Joseph Bagheri <39276421+Josephshark@users.noreply.github.com>
2023-03-12 17:36:45 -0400

    Update main.py
    - Disclaimer help received from chat gpt and the pytorch tutorial

--- main.py
+++ main.py
@@ -15,3 +15,126 @@ Hints:
 import numpy as np
 
 # Your working code here
+# Help received from chat gpt and the pytorch tutorial
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+# Define the neural network architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(nn.functional.relu(self.conv1(x)))
+        x = self.pool(nn.functional.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Define the training and validation datasets
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+# Train the model
+net = Net()
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+train_loss = []
+val_loss = []
+
+number_of_epochs = 2
+
+# Train the network
+for epoch in range(number_of_epochs):  # loop over the dataset multiple times
+    running_training_loss = 0.0
+    for i, train_data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = train_data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_training_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] training loss: %.3f' %
+                  (epoch + 1, i + 1, running_training_loss / 2000))
+            train_loss.append(running_training_loss / 2000)
+            running_training_loss = 0.0
+            # Validate the model
+            with torch.no_grad():
+              running_validation_loss = 0.0
+              for j, test_data in enumerate(testloader,0):
+                inputs, labels = test_data
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                running_validation_loss += loss.item()
+                if j % 2000 == 1999:  # print every 2000 mini-batches
+                    print('[%d, %5d] validation loss: %.3f' %
+                      (epoch + 1, i + 1, running_validation_loss / 2000))
+                    val_loss.append(running_validation_loss / 2000)
+                    running_validation_loss = 0.0
+
+
+print('Finished Training')
+
+
+# Plot the training and validation loss over epochs
+plt.plot(train_loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.xlabel('Epochs (1/12 epochs)')
+plt.ylabel('Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
