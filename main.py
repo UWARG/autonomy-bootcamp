6e71bfdce8a56385f4a0cffd911f346c21e7779b
Joseph Bagheri <39276421+Josephshark@users.noreply.github.com>
2023-03-18 10:12:14 -0400

    Update main.py
    Replaced the old model
    Formatted the code into functions
    Increased the accuracy

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ Hints:
 * We suggest using a convolutional neural network.
 * TensorFlow Keras has the CIFAR-10 dataset as a module, so you don't need to manually download and unpack it.
 """
-
+"""
+Credits
+Chatgpt for the model design and 
+help with the structure of the functions
+"""
 # Import whatever libraries/modules you need
 
 import numpy as np
@@ -22,119 +26,220 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import os
+
+
+# Prepare the data!
+def prepare_data(train_data, val_data, test_data, batch_size, num_workers):
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True,num_workers=num_workers)
+    val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, val_loader, test_loader
+
 
 # Define the neural network architecture
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+def create_model():
+  class CIFARNet(nn.Module):
+      def __init__(self):
+          super(CIFARNet, self).__init__()
+          
+          self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+          self.bn1 = nn.BatchNorm2d(32)
+          self.relu1 = nn.ReLU(inplace=True)
+          self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding=1)
+          self.bn2 = nn.BatchNorm2d(32)
+          self.relu2 = nn.ReLU(inplace=True)
+          self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+          
+          self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+          self.bn3 = nn.BatchNorm2d(64)
+          self.relu3 = nn.ReLU(inplace=True)
+          self.conv4 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1)
+          self.bn4 = nn.BatchNorm2d(64)
+          self.relu4 = nn.ReLU(inplace=True)
+          self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+          
+          self.conv5 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+          self.bn5 = nn.BatchNorm2d(128)
+          self.relu5 = nn.ReLU(inplace=True)
+          self.conv6 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1)
+          self.bn6 = nn.BatchNorm2d(128)
+          self.relu6 = nn.ReLU(inplace=True)
+          self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+          
+          self.fc1 = nn.Linear(in_features=128*4*4, out_features=512)
+          self.bn7 = nn.BatchNorm1d(512)
+          self.relu7 = nn.ReLU(inplace=True)
+          self.dropout = nn.Dropout(p=0.5)
+          
+          self.fc2 = nn.Linear(in_features=512, out_features=10)
+          self.softmax = nn.Softmax(dim=1)
+          
+      def forward(self, x):
+          x = self.conv1(x)
+          x = self.bn1(x)
+          x = self.relu1(x)
+          x = self.conv2(x)
+          x = self.bn2(x)
+          x = self.relu2(x)
+          x = self.pool1(x)
+          
+          x = self.conv3(x)
+          x = self.bn3(x)
+          x = self.relu3(x)
+          x = self.conv4(x)
+          x = self.bn4(x)
+          x = self.relu4(x)
+          x = self.pool2(x)
+          
+          x = self.conv5(x)
+          x = self.bn5(x)
+          x = self.relu5(x)
+          x = self.conv6(x)
+          x = self.bn6(x)
+          x = self.relu6(x)
+          x = self.pool3(x)
+          
+          x = x.view(-1, 128*4*4)
+          x = self.fc1(x)
+          x = self.bn7(x)
+          x = self.relu7(x)
+          x = self.dropout(x)
+          
+          x = self.fc2(x)
+          x = self.softmax(x)
+          
+          return x
 
-    def forward(self, x):
-        x = self.pool(nn.functional.relu(self.conv1(x)))
-        x = self.pool(nn.functional.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = nn.functional.relu(self.fc1(x))
-        x = nn.functional.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+  net = CIFARNet()
+  return net
 
-# Define the training and validation datasets
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Train function
+def train_model(model, train_loader, criterion, optimizer, device):
+  model.train()
+  for inputs, targets in train_loader:
+      inputs, targets = inputs.to(device), targets.to(device)
+      optimizer.zero_grad()
+      outputs = model(inputs)
+      loss = criterion(outputs, targets)
+      loss.backward()
+      optimizer.step()
+  return loss.item()
+# Evaluate model
+def evaluate_model(model, val_loader, criterion, device):
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, targets in val_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            _, predicted = torch.max(outputs.data, 1)
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
+    accuracy = 100 * correct / total
+    return [loss.item(), accuracy]
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+# Define ploting function
+def plot_loss(train_loss, val_loss):
+  # create an array of indices to use as the x-axis
+  num_epochs = len(val_loss, )
+  indices = range(1, num_epochs + 1)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+  # plot the validation losses in blue
+  plt.plot(indices, val_loss, 'b', label='Validation Loss')
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+  # plot the training losses in red
+  plt.plot(indices, train_loss, 'r', label='Training Loss')
 
+  # set axis labels and title
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss')
+  plt.title('Training and Validation Losses')
 
-# Train the model
-net = Net()
+  # add a legend
+  plt.legend()
 
-# Define the loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-train_loss = []
-val_loss = []
-
-number_of_epochs = 2
-
-# Train the network
-for epoch in range(number_of_epochs):  # loop over the dataset multiple times
-    running_training_loss = 0.0
-    for i, train_data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = train_data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_training_loss += loss.item()
-        if i % 2000 == 1999:  # print every 2000 mini-batches
-            print('[%d, %5d] training loss: %.3f' %
-                  (epoch + 1, i + 1, running_training_loss / 2000))
-            train_loss.append(running_training_loss / 2000)
-            running_training_loss = 0.0
-            # Validate the model
-            with torch.no_grad():
-              running_validation_loss = 0.0
-              for j, test_data in enumerate(testloader,0):
-                inputs, labels = test_data
-                outputs = net(inputs)
-                loss = criterion(outputs, labels)
-                running_validation_loss += loss.item()
-                if j % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] validation loss: %.3f' %
-                      (epoch + 1, i + 1, running_validation_loss / 2000))
-                    val_loss.append(running_validation_loss / 2000)
-                    running_validation_loss = 0.0
+  # display the plot
+  plt.show()
 
 
-print('Finished Training')
+def main():
+    # 1. Data preprocessing
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    data_dir = './data'
+
+    download = False
+     # Check if the CIFAR-10 data files exist
+    if not os.path.isdir(data_dir):
+        download = True
+        print("CIFAR-10 dataset not found in the data directory!")
+    train_data = trainset = torchvision.datasets.CIFAR10(root=data_dir, train=True,
+                                                download=download, transform=transform)
+    
+    val_data =  torchvision.datasets.CIFAR10(root=data_dir, train=False,
+                                          download=download, transform=transform)
+    
+    test_data = torchvision.datasets.CIFAR10(root=data_dir, train=False,
+                                          download=download, transform=transform)
+     
+    batch_size = 4
+
+    num_workers = 2
+    
+    train_loader, val_loader, test_loader = prepare_data(train_data, val_data, test_data, batch_size, num_workers)
+    
+    # 2. Model architecture
+    model = create_model()
+    
+    # 3. Compile the model
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    
+    # 4. Train and evaluate the model
+    NUMBER_OF_EPOCHS = 20
+    train_loss=[]
+    val_loss=[]
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    for epoch in range(NUMBER_OF_EPOCHS):
+        train_loss.append(train_model(model, train_loader, criterion, optimizer, device))
+        val_loss_accuracy = evaluate_model(model, val_loader, criterion, device)
+        val_loss.append(val_loss_accuracy[0])
+        print(f"Epoch {epoch+1}, Training loss: {train_loss[epoch]},Validation loss: {val_loss_accuracy[0]}, Validation accuracy: {val_loss_accuracy[1]:.2f}%")
+    
+    torch.save(model.state_dict(), 'my_model.pth')
+
+    
+    # 5. Plot the train and val loss over epochs
+    plot_loss(train_loss, val_loss)
 
 
-# Plot the training and validation loss over epochs
-plt.plot(train_loss, label='Training Loss')
-plt.plot(val_loss, label='Validation Loss')
-plt.xlabel('Epochs (1/12 epochs)')
-plt.ylabel('Loss')
-plt.title('Training and Validation Loss')
-plt.legend()
-plt.show()
+    # 6. Get the final accuracy
+    val_loss_accuracy = evaluate_model(model, val_loader, criterion, device)
+    print(f"Validation accuracy: {val_loss_accuracy[1]:.2f}%")
 
-correct = 0
-total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        # calculate outputs by running images through the network
-        outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
 
-print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+    # 7. Use the model for predictions
+   # with torch.no_grad():
+   #     for inputs, _ in test_loader:
+   #         inputs = inputs.to(device)
+   #         outputs = model(inputs)
+   #         _, predicted = torch.max(outputs.data, 1)
+   #         # do something with the predicted labels
+
+if __name__ == '__main__':
+    main()
